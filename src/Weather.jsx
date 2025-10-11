192e228515d4d08a59f32e74cf5e78e8c7fa06ff
Saniya Mulla <saniyamulla2105@gmail.com>
2025-10-11 17:11:59 +0000

    Updated CSS and jsx

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,7 +1,7 @@
 import { WiThermometer, WiHumidity, WiCloud, WiStrongWind } from "react-icons/wi";
 import { GiFarmer } from "react-icons/gi";
 import React, { useState } from "react";
-import "./weather.css";
+import "./Weather.css";
 
 const VITE_WEATHER_KEY = "78ed0eeee94a2de54804f3e574f7d36c"; // ✅ Replace with your API key
 
